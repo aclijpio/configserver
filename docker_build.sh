102044bd96c8fcd89d90ff60c7a63755e52fad2c
aclij <aclijpio@gmail.com>
2024-03-04 19:35:59 +0300

    eureka server

--- docker_build.sh
+++ docker_build.sh
@@ -3,14 +3,13 @@
 
 mvn clean install
 
-project_build_fileName=$(find target -type f -name '*.jar' | grep -v '/$')
-if [ -z "$project_build_fileName" ]; then
+project_build_path=$(find target -type f -name '*.jar' | grep -v '/$')
+if [ -z "$project_build_path" ]; then
   echo "File not found."
   exit 1
 fi
 
-echo "The file found."
-
-docker build --build-arg JAR_FILE="$project_build_fileName" -t configserver -f Dockerfile .
+project_build_fileName=$(echo "${project_build_path#*target/}" | cut -d '-' -f 1 | tr '[:upper:]' '[:lower:]')
+docker build --build-arg JAR_FILE="$project_build_path" -t "$project_build_fileName" -f Dockerfile .
 
 echo "Docker image built successfully."
